7caadabe9b4e86af59c3727948981b0d86166edf
shieldsy123 <michael.t.shields@gmail.com>
2018-05-03 18:12:28 -0600

    added routes/token

--- routes/token.js
+++ routes/token.js
@@ -1,10 +1,52 @@
-'use strict';
+'use strict'
 
-const express = require('express');
+const express = require('express')
+const humps = require('humps')
+const camelCase = require('camelcase-keys')
+const knex = require('../knex')
+const decamelize = require('decamelize')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 // eslint-disable-next-line new-cap
-const router = express.Router();
+const router = express.Router()
+const key = process.env.JWT_KEY
 
-// YOUR CODE HERE
+router.get('/token', (req, res, next) => {
+  if (!req.cookies.token) {
+    res.json(false)
+  } else {
+    res.json(true)
+  }
+})
 
-module.exports = router;
+router.post('/token', (req, res, next) => {
+  const user = {
+    email: req.body.email
+  }
+  knex('users')
+    .where('email', req.body.email)
+    .then((users) => {
+      if (users.length < 1){
+        res.status(400).type('text/plain').send('Bad email or password')
+      } else {
+        bcrypt.compare(req.body.password, users[0].hashed_password, (err, response) => {
+          if (req.body.email === users[0].email && response) {
+            user.id = users[0].id
+            user.firstName = users[0].first_name
+            user.lastName = users[0].last_name
+            const signedUser = jwt.sign(user, key)
+            res.cookie('token', signedUser, { path: '/', httpOnly: true }).json(user)
+          } else {
+            res.status(400).type('text/plain').send('Bad email or password')
+          }
+        })
+      }
+    })
+})
+
+router.delete('/token', (req, res, next) => {
+  res.cookie('token', '').json(true)
+})
+
+module.exports = router
